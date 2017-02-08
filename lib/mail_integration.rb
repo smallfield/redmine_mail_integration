0a13a551b2883294ca03761b70b28dc14a8e249a
Tomoharu Ono <t4-ono@nri.co.jp>
2017-02-08 11:04:51 +0900

    新規チケット追加時にも、メールのMessage-Idが取り込まれるようにした。

--- lib/mail_integration.rb
+++ lib/mail_integration.rb
@@ -15,12 +15,20 @@ class MailHandler
       #新しいIssueを作成する
       issue = receive_issue
     end
-    # MessageIDの履歴を登録
-    msg = MailMessage.new
-    msg.message_id = email.message_id
-    msg.issue_id = issue.id
-    msg.save!
     issue
   end
   alias_method_chain :dispatch_to_default, :integration
+
+  def dispatch_with_integration
+    issue = dispatch_without_integration
+    if(issue.is_a?(Issue))
+      # MessageIDの履歴を登録
+      msg = MailMessage.new
+      msg.message_id = email.message_id
+      msg.issue_id = issue.id
+      msg.save!
+      logger.info "MailHandler: Message-Id <#{email.message_id}> is correlated to issue ##{issue.id}"
+    end
+  end
+  alias_method_chain :dispatch, :integration
 end
