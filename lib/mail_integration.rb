662c7bf7c020520663ae727364f19f7d7ef7f278
Tomoharu Ono <rio.cabon@gmail.com>
2017-02-24 21:31:44 +0900

    Fixes bug that task failure when recieved email doesn't have a Cc.

--- lib/mail_integration.rb
+++ lib/mail_integration.rb
@@ -4,15 +4,15 @@ require_dependency "mail_handler"
 
 class MailHandler
   def dispatch_to_default_with_integration
-    # 過去のメッセージIDの検索
+    # find from saved Message-Id
     old_msg = MailMessage.find_by_message_id(email.message_id)
     old_msg = MailMessage.find_by_message_id(email.references) unless old_msg
     if old_msg
-      #過去のIssueを更新する
+      # Update existing issue
       journal = receive_issue_reply(old_msg.issue_id)
       issue = journal.issue
     else
-      #新しいIssueを作成する
+      # create new issue
       issue = receive_issue
     end
     issue
@@ -22,7 +22,7 @@ class MailHandler
   def dispatch_with_integration
     issue = dispatch_without_integration
     if(issue.is_a?(Issue))
-      # MessageIDの履歴を登録
+      # Save Message-Id
       msg = MailMessage.new
       msg.message_id = email.message_id
       msg.issue_id = issue.id
@@ -33,12 +33,15 @@ class MailHandler
   alias_method_chain :dispatch, :integration
 
   def cleanup_body_with_integration(body)
-    "<pre>From: #{email.from.join(",")}\nTo: #{email.to.join(",")}\nCc: #{email.cc.join(",")}\nSubject: #{email.subject.to_s}\n\n#{cleanup_body_without_integration(body)}</pre>"
+    from = email.from.to_a.join(", ")
+    to = email.to.to_a.join(", ")
+    cc = email.cc.to_a.join(", ")
+    "~~~\n   From: #{from}\n     To: #{to}\n     Cc: #{cc}\nSubject: #{email.subject.to_s}\n\n#{cleanup_body_without_integration(body)}\n~~~"
   end
   alias_method_chain :cleanup_body, :integration
 end
 
-# Issueモデルへのパッチ当て
+# Issue model patching
 module IssuePatch
   def self.included(base)
     base.class_eval do
